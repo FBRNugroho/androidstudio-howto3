5c40cd386559929aa1fdb40dbe26d906bc4bbd7d
Febrianto Nugroho <id@febriantonugroho.com>
2017-06-15 05:02:04 +0700

    no message

--- app/src/main/java/com/example/febriantonugroho/androidstudio_howto3/MainActivity.java
+++ app/src/main/java/com/example/febriantonugroho/androidstudio_howto3/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.febriantonugroho.androidstudio_howto3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Todo: to Portrait Activity */
+        findViewById(R.id.btnPortrait).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPortrait = new Intent(getBaseContext(), PortraitActivity.class);
+                startActivity(intentPortrait);
+            }
+        });
+
+        /* Todo: to Landscape Activity */
+        findViewById(R.id.btnLandscape).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLandscape = new Intent(getBaseContext(), LandscapeActivity.class);
+                startActivity(intentLandscape);
+            }
+        });
     }
 }
